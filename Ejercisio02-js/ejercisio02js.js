637f1fbd48d2dbc5356d81893c368efc04a8b504
Lucas Sanchez <42994495lucas@gmail.com>
2022-09-14 12:26:19 -0300

    Ejercisio 02 finalizado

---
+++ Ejercisio02-js/ejercisio02js.js
@@ -0,0 +1,7 @@
+const nacimiento = new Date(2000, 12, 17);
+const librofav = {
+  titulo: "El toque de Midas",
+  autor: "Donald J Trump",
+  fechalanzamiento: 2011,
+};
+var meinfo = ["Lucas Waldemar Sanchez", 21, true, nacimiento, librofav];
